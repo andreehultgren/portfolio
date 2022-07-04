0ee1262a1fdfc07bf3b1acd7f5ddf183f95a9516
Andree Hultgren <andree.hultgren+github@gmail.com>
2022-07-04 22:13:38 +0200

    add build

---
+++ build/_app/immutable/pages/backend.svelte-fbdb167d.js
@@ -0,0 +1,1 @@
+import{S as r,i,s as l,e as d,t as h,c as m,a as p,h as _,d as c,g as f,G as u,n as s}from"../chunks/index-3172f7ba.js";function x(o){let e,n;return{c(){e=d("h1"),n=h("Backend")},l(a){e=m(a,"H1",{});var t=p(e);n=_(t,"Backend"),t.forEach(c)},m(a,t){f(a,e,t),u(e,n)},p:s,i:s,o:s,d(a){a&&c(e)}}}class B extends r{constructor(e){super(),i(this,e,null,x,l,{})}}export{B as default};
---
+++ build/_app/immutable/pages/cloud.svelte-c11fde9f.js
@@ -0,0 +1,1 @@
+import{S as r,i,s as c,e as d,t as h,c as u,a as m,h as p,d as o,g as _,G as f,n}from"../chunks/index-3172f7ba.js";function x(l){let e,s;return{c(){e=d("h1"),s=h("Cloud")},l(t){e=u(t,"H1",{});var a=m(e);s=p(a,"Cloud"),a.forEach(o)},m(t,a){_(t,e,a),f(e,s)},p:n,i:n,o:n,d(t){t&&o(e)}}}class g extends r{constructor(e){super(),i(this,e,null,x,c,{})}}export{g as default};
